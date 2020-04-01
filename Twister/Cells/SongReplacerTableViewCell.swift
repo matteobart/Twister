186ad2c19b15f8a868b03d49c1e13282a0c692ac
Matteo Bartalini <MatteoBartalini1@gmail.com>
2020-03-31 20:59:50 -0400

    Complete transition from base code + updated readme

---
+++ Twister/Cells/SongReplacerTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  SongReplacerTableViewCell.swift
+//  
+//
+//  Created by Matteo Bart on 1/20/20.
+//
+
+import UIKit
+
+class SongReplacerTableViewCell: UITableViewCell {
+    static let identifier = "SongCell"
+    
+    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var albumLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Twister/Controllers/SongReplacerViewController.swift
@@ -0,0 +1,78 @@
+//
+//  SongReplacerViewController.swift
+//  Adding-Content-to-Apple-Music
+//
+//  Created by Matteo Bart on 1/20/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class SongReplacerViewController: UIViewController {
+
+    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var songTableView: UITableView!
+    @IBOutlet weak var albumLabel: UILabel!
+    
+    var resultsVC: ResultsViewController?
+    
+    var originalSongData: (name: String, artist: String, album: String)?
+    var index: Int? // index of the song from the previous frame
+    
+    var replacementSongs: [Song] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let originalSongData = originalSongData else { return }
+        songTableView.dataSource = self
+        songTableView.delegate = self
+        songLabel.text = originalSongData.name
+        artistLabel.text = originalSongData.artist
+        albumLabel.text = originalSongData.album
+        
+        // Do any additional setup after loading the view.songVC
+    }
+    
+        
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension SongReplacerViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return replacementSongs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SongReplacerTableViewCell.identifier,
+                                                       for: indexPath) as? SongReplacerTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.artistLabel.text = replacementSongs[indexPath.item].artist
+        cell.songLabel.text = replacementSongs[indexPath.item].name
+        cell.albumLabel.text = replacementSongs[indexPath.item].album
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let resultsVC = resultsVC else { return }
+        guard let index = index else { return }
+        resultsVC.addToPlaylist(song: replacementSongs[indexPath.item], index: index)
+        dismiss(animated: true) {
+            
+        }
+    }
+    
+    
+    
+}
